b31ef4ce6b227609f48c44e5155bb7f77859d058
fcosoftware <fcosoftware35@gmail.com>
2023-11-12 14:53:28 -0300

    Criando o muno Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,23 @@
+import { Link } from 'react-router-dom'
+
 import Logo from '../../assets/logo.png'
-import { Container } from './styles'
+import { Container, Menu, Li } from './styles'
 
 function Header() {
   return (
     <Container>
-      <img src={Logo} alt="logo-dev-movies" style={{ width: 500 }} />
+      <img src={Logo} alt="logo-dev-movies" />
+      <Menu>
+        <Li>
+          <Link to="/">Home</Link>
+        </Li>
+        <Li>
+          <Link to="/filmes">Filmes</Link>
+        </Li>
+        <Li>
+          <Link to="/series">Séries</Link>
+        </Li>
+      </Menu>
     </Container>
   )
 }
